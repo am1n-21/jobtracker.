a833c649ee036cba8690741fdc15f1241f802590
Shwan Amin <z5684089@ad.unsw.edu.au>
2026-02-26 15:38:51 +1100

    fix

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(session({
 
 // ROOT ROUTE
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'landing.html'));
 });
 
 // ROUTERS
