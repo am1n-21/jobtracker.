a5299bc9bba79a25f6838ca825995ef03eeb3615
Shwan Amin <z5684089@ad.unsw.edu.au>
2026-02-26 15:33:05 +1100

    fix

--- server.js
+++ server.js
@@ -2,26 +2,28 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import session from 'express-session';
-import cors from 'cors'
+import cors from 'cors';
+import { fileURLToPath } from 'url';
+import path from 'path';
 dotenv.config();
 
 import { appRouter } from './routes/app.js';
 import { authRouter } from './routes/auth.js';
-import { profileRouter } from './routes/profile.js'
-
-import { createTables, viewUsersTable, viewApplicationsTable } from './db/db.js';
 import { profileRouter } from './routes/profile.js';
+import { createTables } from './db/db.js';
 
 // CONSTS
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 3000;
 const app = express();
-app.use(cors());
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-createTables()
+app.use(cors());
+createTables();
 
 // MIDDLEWARE
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
@@ -30,11 +32,12 @@ app.use(session({
 
 // ROOT ROUTE
 app.get('/', (req, res) => {
-    res.sendFile('landing.html');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// SERVER
+// ROUTERS
 app.use('/', appRouter);
 app.use('/auth', authRouter);
 app.use('/profile', profileRouter);
-app.listen(PORT, () => console.log('Listening on port'));
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
